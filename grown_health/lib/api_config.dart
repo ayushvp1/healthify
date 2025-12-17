07ea346662a7b09ada5e93667efd873f3d9d4012
govindhansv <govindhansv333@gmail.com>
2025-12-17 17:07:00 +0530

    new

--- grown_health/lib/api_config.dart
+++ grown_health/lib/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // static const String baseUrl = 'https://healthify-api.vercel.app/api';
   // static const String baseUrl = 'https://ayush-healthify-temp3.loca.lt/api';
-  static const String baseUrl = 'http://localhost:4000/api';
+  static const String baseUrl = 'http:localhost:4000/api';
 }
 
